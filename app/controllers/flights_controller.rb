85cdaf02666d8409e46dcc163051f54e33af3fb7
tomokoichikawa <takator@yahoo.co.jp>
2019-10-15 16:49:57 +0900

    共通化

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
+  before_action :set_flight, only: [:show, :edit, :update]
+
   def index
   end
 
@@ -36,5 +38,9 @@ class FlightsController < ApplicationController
 
   def flight_params
   	params.require(:flight).permit(:departure_date, :airline, :flight_number, :aircraft_type, :seat_class, :seat_number, :departure_airport, :departure_estimated_time, :actual_departure_time, :arrival_airport, :arrival_estimated_time, :actual_arrival_time, :flight_time, :flight_mile, :fare, :booking_class, :getting_mile, :getting_point, :review, :photo1, :photo2, :photo3, :total_getting_mile, :total_getting_point)
+  end
+
+  def set_flight
+    @flight = Flight.find(params[:id])
   end
 end
