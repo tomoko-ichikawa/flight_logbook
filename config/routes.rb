5dd622e0d2b17d830385d246a801acef6bc258ae
tomokoichikawa <takator@yahoo.co.jp>
2019-10-30 16:15:48 +0900

    Add:profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'flights#index'
   
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+}
 
   resources :flights do
   	collection do
